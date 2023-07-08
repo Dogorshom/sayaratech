8b82db33822e93fa9bcb81d3cbda5df902324af9
Dogorshom <saloom.dogorshom@gmail.com>
2023-07-08 17:34:51 +0300

    Document code

--- lib/controllers/tokens/refresh_token.dart
+++ lib/controllers/tokens/refresh_token.dart
@@ -5,22 +5,35 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../models/authentication.dart';
 
+/// Refresh Token when expired
+///
+/// Return `true` if token refreshed successfully and `false` otherwise
 Future<bool> refreshToken() async {
+  //get all authentication variables
   Authentication authVars = Get.put(Authentication());
   try {
+    //Request's URL
     Uri url = Uri.parse("https://satc.live/api/Auth/Token/refreshToken");
+    //Header for post request, pass previous token
     Map<String, String> headers = {'token': authVars.bearerToken.value};
+    //'Post' request
     http.Response response = await http.post(url, headers: headers);
+    //Decode json data
     Map dataRecieved = jsonDecode(response.body);
-    if (dataRecieved["status"]) {
+    //Check if the 'status' of the request is true or not
+    if (dataRecieved["status"] != null && dataRecieved["status"] == true) {
+      //Shared Preferences to save new token locally
       SharedPreferences sharedPreferences =
           await SharedPreferences.getInstance();
+      //Save new token just in this session
       authVars.bearerToken.value = dataRecieved['Data']['token'];
+      //Save locally to the device
       sharedPreferences.setString(
           "customer_latest_token", authVars.bearerToken.value);
       return true;
     }
   } on Exception catch (e) {
+    //log the error
     log(e.toString());
     return false;
   }
